3cd54f92481a3c5efcf1e25f61647cc4ffb23c3c
Julio Miranda <uxdesign@naturalizou.com.br>
2020-10-26 23:30:43 -0300

    Bug resolvido graças a Deus, pena que sem tempo para terminar. faltando apenas 44:20 minutos de aula fico a prosseguir com o video #5 em 1:15:58

--- src/pages.js
+++ src/pages.js
@@ -1,4 +1,5 @@
-const orphanages = require('./database/fakedata.js');
+const Database = require('./database/db');
+const saveOrphanage = require('./database/saveOrphanage');
 
 module.exports = {
     
@@ -7,12 +8,33 @@ module.exports = {
         return response.render('index')
     },
 
-    orphanage (request, response) {
-        return response.render('orphanage')
+    async orphanage (request, response) {
+        const id = request.query.id
+        try {
+            const db = await Database;
+            const results  = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+            const orphanage = results[0]
+
+            orphanage.images = orphanage.images.split(",")
+            orphanage.firstImage = orphanage.images[0]
+
+            return response.render('orphanage', { orphanage })
+        }catch (error) { 
+            console.log(error)
+            return response.send('Erro no banco de dados!')
+        }
+        
     },
 
-    orphanages (request, response) {
-        return response.render('orphanages', { orphanages })
+    async orphanages (request, response) {
+        try { 
+            const db = await Database;
+            const orphanages = await db.all("SELECT * FROM orphanages")
+            return response.render('orphanages', { orphanages })
+        } catch (error) {
+            console.log(error)
+            return response.send('Erro no banco de dados!')
+        }
     },
 
     createOrphanage (request, response) {
